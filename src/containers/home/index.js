ab0ae99c10521b8d7f19987f3672ff65ca5493c7
Su Ho V <su.ho@asiantech.vn>
2017-09-07 16:33:22 +0700

    remove redux

---
+++ src/containers/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Button } from "react-native";
+import Define from "../../define";
+
+export default class Splash extends Component {
+  state = {};
+  render() {
+    return (
+      <View style={Define.Styles.containerCentered}>
+        <Button onPress={() => {}} title="Back To Splash" />
+      </View>
+    );
+  }
+}
