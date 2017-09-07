b87bcc992426fed8c68db5681cf0c1c8c1dc6088
Su Ho V <su.ho@asiantech.vn>
2017-09-07 20:21:22 +0700

    add rn router flux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import Splash from "./containers/splash";
+import { Router } from "react-native-router-flux";
 
 export default class ReactNativeDemo extends Component {
   render() {
