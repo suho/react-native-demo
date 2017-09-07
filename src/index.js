f85dfa4d4d266d4da52a1d68504e5630a89c4521
Su Ho V <su.ho@asiantech.vn>
2017-09-07 11:01:46 +0700

    remove redux

--- src/index.js
+++ src/index.js
@@ -7,95 +7,14 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 // State
-let appState = { number: 1, errorMessage: "" };
 
 // Action
-const add = {
-  type: "ADD",
-  value: 1
-};
-
-const sub = {
-  type: "SUB",
-  value: 1
-};
 
 // Reducer
-const numberReducer = (state = appState, action) => {
-  switch (action.type) {
-    case "ADD":
-      state = {
-        ...state,
-        number: state.number + action.value
-      };
-      break;
-    case "SUB":
-      state = {
-        ...state,
-        number: state.number - action.value
-      };
-      break;
-  }
-  return state;
-};
-
-const errorReducer = (state = appState, action) => {
-  switch (action.type) {
-    case "GREATER_THAN_FIVE":
-      state = {
-        ...state,
-        errorMessage: "Number can not be greater than five"
-      };
-      break;
-  }
-  return state;
-};
 
 // Middleware
-const logger = store => next => action => {
-  console.log("State", store.getState());
-  next(action);
-  console.log("State Updated", store.getState());
-  alert("Status Updated", store.getState());
-};
-
-const checkNumber = store => next => action => {
-  const number = store.getState().number.number;
-  if (number > 5) {
-    next({ type: "GREATER_THAN_FIVE" });
-  } else {
-    next(action);
-  }
-  console.log("Current Number", number);
-};
 
 // Store
-const reducers = combineReducers({
-  number: numberReducer,
-  error: errorReducer
-});
-
-const store = createStore(
-  reducers,
-  applyMiddleware(thunk, logger, checkNumber)
-);
-
-// Test
-// store.subscribe(() => {
-//   console.log("State Updated", store.getState());
-// });
-
-// store.dispatch(add);
-
-const addAfter3s = () => {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch(add);
-    }, 3000);
-  };
-};
-
-store.dispatch(addAfter3s());
 
 export default class ReactNativeDemo extends Component {
   constructor(props) {
