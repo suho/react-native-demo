f2b4fee6c941ff9f78576dda13ebe626e307ea89
Su Ho V <su.ho@asiantech.vn>
2017-09-07 15:49:29 +0700

    - add react-redux

--- src/index.js
+++ src/index.js
@@ -5,58 +5,94 @@ import TestRow from "./components/rows/test-row";
 import FoodsView from "./components/list-view";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
 // State
-
-// Action
+let appState = {
+  menus: [
+    {
+      id: -1,
+      item: {
+        cover: "https://unsplash.it/300/300",
+        name: "Test"
+      },
+      price: {
+        unitPrice: 10000
+      }
+    }
+  ]
+};
 
 // Reducer
+const foodsReducer = (state = appState, action) => {
+  switch (action.type) {
+    case "LOAD_FOODS":
+      state = {
+        ...state,
+        menus: [
+          {
+            id: -1,
+            item: {
+              cover: "https://unsplash.it/300/300",
+              name: "Test"
+            },
+            price: {
+              unitPrice: 10000
+            }
+          },
+          {
+            id: -2,
+            item: {
+              cover: "https://unsplash.it/300/300",
+              name: "Test Test"
+            },
+            price: {
+              unitPrice: 20000
+            }
+          }
+        ]
+      };
+      break;
+    case "CLEAR_FOODS":
+      state = {
+        ...state,
+        menus: []
+      };
+      break;
+  }
+  return state;
+};
 
 // Middleware
 
 // Store
+const store = createStore(foodsReducer, appState);
+
+const loadMenus = () => {
+  fetch("http://104.154.229.21/api/v1/shops/1")
+    .then(response => response.json())
+    .then(json => {
+      let data = json.data;
+      return data.menus[0].menuItems;
+    })
+    .catch(error => {
+      alert(error);
+    });
+};
 
 export default class ReactNativeDemo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      menus: []
+      menus: appState.menus
     };
-  }
-
-  loadServices() {
-    return fetch("http://104.154.229.21/api/v1/shops/1")
-      .then(response => response.json())
-      .then(json => {
-        let data = json.data;
-        this.setState({
-          menus: data.menus[0].menuItems
-        });
-      })
-      .catch(error => {
-        alert(error);
-      });
   }
 
   render() {
     return (
-      <View style={[Define.Styles.container, Define.Styles.containerCentered]}>
-        <Button
-          onPress={() => {
-            this.loadServices();
-          }}
-          title={Define.Strings.button.title.loadServices}
-        />
-        <Button
-          onPress={() => {
-            this.setState({
-              menus: []
-            });
-          }}
-          title={Define.Strings.button.title.clearData}
-        />
-        <FoodsView menus={this.state.menus} />
-      </View>
+      <Provider store={store}>
+        <FoodsView />
+      </Provider>
     );
   }
 }
